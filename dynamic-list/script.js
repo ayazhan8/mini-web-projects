65a71c518fb7bab6778b6533d488e72bb65a8208
ayazhan8 <54767127+ayazhan8@users.noreply.github.com>
2022-01-28 20:09:05 +0600

    basic layout

---
+++ dynamic-list/script.js
@@ -0,0 +1,5 @@
+const description = document.createElement("p");
+description.textContent =
+  "This is a dynamic list. Click anywhere to add a list item. Click on a list item to rename it.";
+
+document.body.appendChild(description);
